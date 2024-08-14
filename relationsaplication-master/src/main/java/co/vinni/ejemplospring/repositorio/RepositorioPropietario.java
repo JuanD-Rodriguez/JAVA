31d6197bff0510b22f68a22566df97a437c815c1
JuanD-Rodriguez <juandavidrh02@gmail.com>
2024-08-14 09:45:55 -0500

    Delete relationsaplication-master directory

--- relationsaplication-master/src/main/java/co/vinni/ejemplospring/repositorio/RepositorioPropietario.java
+++
@@ -1,7 +0,0 @@
-package co.vinni.ejemplospring.repositorio;
-
-import co.vinni.ejemplospring.entidades.Propietario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RepositorioPropietario extends JpaRepository<Propietario, Integer> {
-}
--- relationsaplication-master/src/main/resources/cargacarros.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO carros (modelo, placa) VALUES (2021, 'ABC987');
-INSERT INTO carros (modelo, placa) VALUES (2020, 'SWA957');
-INSERT INTO carros (modelo, placa) VALUES (2018, 'TYT980');
